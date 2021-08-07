2e7d8968f942a7b2e7ef58e6d7b640f2232840de
Felipe Pedroso <f.pedroso677@gmail.com>
2021-08-07 19:19:33 +0100

    Creating CitiesSearchFragment.

---
+++ app/src/main/kotlin/br/pedroso/countrieslist/features/citiessearch/CitiesSearchFragment.kt
@@ -0,0 +1,28 @@
+package br.pedroso.countrieslist.features.citiessearch
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.pedroso.countrieslist.databinding.FragmentCitiesSearchBinding
+
+class CitiesSearchFragment : Fragment() {
+
+    private var _binding: FragmentCitiesSearchBinding? = null
+    private val binding: FragmentCitiesSearchBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCitiesSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
