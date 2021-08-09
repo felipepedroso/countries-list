52366f0b91d5074469591f66c52d97cdd6ebd75c
Felipe Pedroso <f.pedroso677@gmail.com>
2021-08-09 21:18:35 +0100

    Implementing getCities method.

--- app/src/main/kotlin/br/pedroso/citieslist/datasource/JsonResourceCitiesDataSource.kt
+++ app/src/main/kotlin/br/pedroso/citieslist/datasource/JsonResourceCitiesDataSource.kt
@@ -1,10 +1,14 @@
 package br.pedroso.citieslist.datasource
 
+import br.pedroso.citieslist.R
 import br.pedroso.citieslist.datasource.rawresourcereader.RawResourceReader
 import br.pedroso.citieslist.domain.datasource.CitiesDataSource
 import br.pedroso.citieslist.domain.datasource.dtos.CityDTO
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 class JsonResourceCitiesDataSource(
     private val rawResourceReader: RawResourceReader,
@@ -12,7 +16,13 @@ class JsonResourceCitiesDataSource(
 ) : CitiesDataSource {
 
     override suspend fun getCities(): List<CityDTO> {
-        TODO()
+        val resourceContent = rawResourceReader.readResourceAsString(R.raw.cities)
+
+        return withContext(dispatcher) {
+            runCatching { Json.decodeFromString<List<CityDTO>>(resourceContent) }
+                .onFailure { error -> throw FailedToDecodeJsonException(error) }
+                .getOrThrow()
+        }
     }
 
     class FailedToDecodeJsonException(cause: Throwable) : Throwable(cause)
