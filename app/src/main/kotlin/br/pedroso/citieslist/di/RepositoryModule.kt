1dad7ca36fe269418c13976779c42746f6de007d
Felipe Pedroso <f.pedroso677@gmail.com>
2021-08-10 17:41:05 +0100

    Wiring KotlinFilterCitiesRepository to Hilt.

--- app/src/main/kotlin/br/pedroso/citieslist/di/RepositoryModule.kt
+++ app/src/main/kotlin/br/pedroso/citieslist/di/RepositoryModule.kt
@@ -1,38 +1,19 @@
 package br.pedroso.citieslist.di
 
-import br.pedroso.citieslist.domain.entities.City
-import br.pedroso.citieslist.domain.entities.Coordinates
+import br.pedroso.citieslist.domain.datasource.CitiesDataSource
 import br.pedroso.citieslist.domain.repository.CitiesRepository
+import br.pedroso.citieslist.repository.KotlinFilterCitiesRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import kotlinx.coroutines.delay
-import kotlin.random.Random
 
 @Module
 @InstallIn(ViewModelComponent::class)
 object RepositoryModule {
 
     @Provides
-    fun provideCitiesRepository(): CitiesRepository {
-        // Injecting a fake object to test the UI.
-        // This will be replaced by a real repository in the future.
-        return object : CitiesRepository {
-            override suspend fun getCities(searchQuery: String): List<City> {
-                delay(2000)
-                return (1..1000).map { index ->
-                    City(
-                        id = index,
-                        name = "City $index",
-                        countryCode = "XX",
-                        coordinates = Coordinates(
-                            latitude = Random.nextDouble(-90.0, 90.0),
-                            longitude = Random.nextDouble(-180.0, 180.0)
-                        )
-                    )
-                }
-            }
-        }
+    fun provideCitiesRepository(citiesDataSource: CitiesDataSource): CitiesRepository {
+        return KotlinFilterCitiesRepository(citiesDataSource)
     }
 }
