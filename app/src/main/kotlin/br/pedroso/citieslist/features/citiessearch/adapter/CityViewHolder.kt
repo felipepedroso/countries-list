710fa8bf9b9269720537004ba86a15e2c5c943e1
Felipe Pedroso <f.pedroso677@gmail.com>
2021-08-10 22:45:57 +0100

    Making the list render the countries flags.

--- app/src/main/kotlin/br/pedroso/citieslist/features/citiessearch/adapter/CityViewHolder.kt
+++ app/src/main/kotlin/br/pedroso/citieslist/features/citiessearch/adapter/CityViewHolder.kt
@@ -1,5 +1,6 @@
 package br.pedroso.citieslist.features.citiessearch.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -11,8 +12,11 @@ class CityViewHolder(
     private val cityOnClickListener: CityOnClickListener
 ) : RecyclerView.ViewHolder(binding.root) {
 
+    // Since this is a prototype, I'm suppressing this warning as we don't need to care about using
+    // string resources for now.
+    @SuppressLint("SetTextI18n")
     fun bind(city: City) {
-        binding.titleTextView.text = "${city.name} (${city.countryCode})"
+        binding.titleTextView.text = "${city.name} ${getCountryFlagEmoji(city.countryCode)}"
         binding.coordinatesTextView.text =
             "${city.coordinates.latitude}, ${city.coordinates.longitude}"
 
@@ -21,7 +25,20 @@ class CityViewHolder(
         }
     }
 
+    // This code is based on this StackOverflow question:
+    // https://stackoverflow.com/questions/42234666/get-emoji-flag-by-country-code
+    private fun getCountryFlagEmoji(countryCode: String): String {
+        val firstChar = countryCode[0].code - ASCII_OFFSET + FLAG_OFFSET
+
+        val secondChar = countryCode[1].code - ASCII_OFFSET + FLAG_OFFSET
+
+        return (String(Character.toChars(firstChar)) + String(Character.toChars(secondChar)))
+    }
+
     companion object {
+        private const val ASCII_OFFSET = 0x41
+        private const val FLAG_OFFSET = 0x1F1E6
+
         fun create(parent: ViewGroup, cityOnClickListener: CityOnClickListener): CityViewHolder {
             val layoutInflater = LayoutInflater.from(parent.context)
             val binding = ItemCityBinding.inflate(layoutInflater, parent, false)
