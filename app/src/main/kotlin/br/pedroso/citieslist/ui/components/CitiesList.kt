6556ce72981b1bb06cb92744d3b016c4dd94d9c1
Felipe Pedroso <f.pedroso677@gmail.com>
2024-01-16 11:53:26 +0000

    Adding a cities counter at the top of the list.

--- app/src/main/kotlin/br/pedroso/citieslist/ui/components/CitiesList.kt
+++ app/src/main/kotlin/br/pedroso/citieslist/ui/components/CitiesList.kt
@@ -1,17 +1,26 @@
 package br.pedroso.citieslist.ui.components
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.pedroso.citieslist.R
 import br.pedroso.citieslist.domain.entities.City
 import br.pedroso.citieslist.domain.entities.Coordinates
 import br.pedroso.citieslist.ui.theme.CitiesListTheme
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CitiesList(
     cities: List<City>,
@@ -19,7 +28,21 @@ fun CitiesList(
     onCityClick: (city: City) -> Unit = {}
 ) {
     LazyColumn(modifier = modifier) {
-        itemsIndexed(cities) { index, city: City ->
+        item {
+            Surface(modifier = Modifier.fillMaxWidth()) {
+                Box(modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)) {
+                    Text(
+                        text = pluralStringResource(
+                            id = R.plurals.query_result,
+                            count = cities.size,
+                            cities.size
+                        )
+                    )
+                }
+            }
+        }
+
+        itemsIndexed(cities, key = { _, city -> city.id }) { index, city: City ->
             CityItem(
                 modifier = Modifier.fillMaxWidth(),
                 city = city,
